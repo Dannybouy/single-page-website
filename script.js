c045458857b1765f6e1da96e929278d16b08ba09
Dannybouy <danielokpara29@gmail.com>
2023-01-16 12:51:16 +0100

    New update

--- script.js
+++ script.js
@@ -15,23 +15,5 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", 
 
 
 
-// Code for Image changing
-const imagesUrls = [
-    "/images/photo_2022-08-19_14-25-55.jpg",
-    "/images/photo_2022-08-19_14-26-18.jpg",
-    "/images/photo_2022-08-19_14-26-22.jpg",
-    "/images/photo_2022-08-19_14-26-26.jpg",
-    "/images/photo_2022-08-19_14-26-38.jpg",
-    "/images/photo_2022-08-19_14-26-45.jpg"
-];
-
-let activeImage = document.getElementById("active-image")
-console.log(activeImage.src)
-
-window.addEventListener("scroll", () => {
-    let activeImageIndex = Math.floor(window.scrollY / (window.innerHeight) /imagesUrls.length)
-    activeImage.src = imagesUrls[activeImageIndex]
-    
-})
 
 
